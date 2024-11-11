e8b1aa50c9f7acb72ad1e7a07e4a6926ba52bbf9
aakashraj1174 <aakashrajdhami174gmail.com>
2024-11-11 20:33:31 +0545

    added my new file akash.py

---
+++ core/aakash.py
@@ -0,0 +1,15 @@
+print("******** Welcome to aakash **********")
+
+x = int (input ("1. What's X ?: "))
+y = int (input("2. what's Y ?: "))
+
+if x < y :
+    print ("Ans = x is less than y")
+elif x > y:
+    print ("x is greater than y")
+elif x == y:
+    print("x = y")
+
+
+name = str(input("What is your name ?: "))
+print(f"My name is {name}.")
