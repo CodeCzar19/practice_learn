9fc16c2b4a85d34d86d13f7628ff7a0b3ed0b4ae
CodeCzar19 <magaryogesh366@gmail.com>
2024-11-28 05:46:37 -0500

    task completed

---
+++ yogesh/cli_app.py
@@ -0,0 +1,4 @@
+name = str(input("What is your name? : "))
+github_username = str(input("What is your github username? : "))
+
+print(f"Hey, My name is {name} and My github username is {github_username}.")
