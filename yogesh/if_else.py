8866032719774b3284d050f2e47feebabedc4b5e
CodeCzar19 <magaryogesh366@gmail.com>
2024-11-29 12:17:25 -0500

    task 2 completed

---
+++ yogesh/if_else.py
@@ -0,0 +1,8 @@
+name = str(input("What is your name? : "))
+age = int(input("What is your age? : "))
+
+if age < 18 :
+    print("You are underage. Please exit.")
+
+else :
+    print(f"You are welcome, {name} to adult party.")   
